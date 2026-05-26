5e93aceae0875993c0febd598696b9c9a4d41579
Khushisinghal-1 <khushisinghal081@gmail.com>
2026-05-26 09:39:45 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -3,12 +3,13 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
 
-const PORT = process.env.PORT || 4000
 const app = express()
-await connectDB()
 
 app.use(express.json())
 app.use(cors())
+
+// Connect DB
+connectDB()
 
 app.get('/', (req, res) => res.send('API Working'))
 
